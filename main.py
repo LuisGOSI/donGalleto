3e8adfd07a5dc08435bde6898a682bbda4d091f2
Danlei311 <armandodanielr29@gmail.com>
2025-03-23 19:07:41 -0600

    arreglo de direccionamiento a crud clientes

--- main.py
+++ main.py
@@ -255,6 +255,41 @@ def cliente_dashboard():
     print(data)
     return render_template('/client/Cliente.html', is_base_template = False,user=user,data=data)
 
+@app.route("/clientes")
+def clientes():
+    if "user" not in session:
+        return redirect(url_for("login"))
+    
+    active_user = session.get("user")
+
+    if active_user[4] != "administrador":
+        return render_template("pages/error404.html"), 404
+
+    status = request.args.get("status", default=1, type=int)  # Por defecto activos
+    
+    cur = mysql.connection.cursor()
+    
+    cur.execute("""
+        SELECT 
+            c.idCliente,
+            c.nombreCliente,
+            c.telefono,
+            u.email,
+            u.rol,
+            u.status
+        FROM 
+            clientes c
+        INNER JOIN 
+            usuarios u ON c.idCliente = u.idClienteFK
+        WHERE 
+            u.status = %s;
+    """, (status,))
+    
+    clientes = cur.fetchall()
+    cur.close()
+    
+    return render_template('/admin/gestionClientes.html', clientes=clientes, status=status, is_base_template=False)
+
 
 @app.route("/carrito")
 def carrito_dashboard():
