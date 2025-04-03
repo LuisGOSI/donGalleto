307693de8312837973122cab72515168ea893d20
Luis Ignacio Gonzalez Silva <luis_gosi@outlook.com>
2025-04-03 17:28:34 -0600

    Merge

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from database.usuario import usuariosCRUD
 from database.production import inventarioDeGalletas, inventarioDeInsumos
 from database.cliente import clientes
 from database.cookies import cookies
-from database.sales import ventas
-from database.sales import corteVenta
+from database.sales import ventas, corteVenta 
 from datetime import datetime
 from db import app, mysql
 from sessions import *
